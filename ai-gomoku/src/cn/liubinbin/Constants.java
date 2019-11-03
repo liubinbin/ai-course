700f284fadd1a6c93819afc2ec99279d70d08337
liubinbin <liubinbean@163.com>
2019-11-03 22:05:50 +0800

    add file

---
+++ ai-gomoku/src/cn/liubinbin/Constants.java
@@ -0,0 +1,16 @@
+package cn.liubinbin;
+
+public class Constants {
+
+    public static final Integer zero = 0;
+    public static final Integer huoyi = 20;
+    public static final Integer siyi = 4;
+    public static final Integer huoer = 400;
+    public static final Integer sier = 90;
+    public static final Integer huosan = 6000;
+    public static final Integer sisan = 800;
+    public static final Integer huosi = 20000;
+    public static final Integer sisi = 10000;
+    public static final Integer wulian = 50000;
+
+}
