108c0e0be14f4646249d0e92187ec4baa5c9eb3b
liubinbin <liubinbean@163.com>
2019-11-12 17:23:19 +0800

    reconginze structure

---
+++ diynn/src/main/java/cn/liubinbin/DIYNeuralNetwork.java
@@ -0,0 +1,12 @@
+package cn.liubinbin;
+
+/**
+ * Created by bin on 2019/11/12.
+ *
+ * @Description: TODO
+ */
+public class DIYNeuralNetwork {
+    public static void main(String[] args) {
+        System.out.println("HELLO DIYNeuralNetwork");
+    }
+}
